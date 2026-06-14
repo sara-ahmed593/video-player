07538ffb72367dca07043ad13278e52cf7e97b41
Sara Ahmed <sara.ahmed@ad.cyshield>
2026-06-14 13:11:56 +0300

    duplication

--- src/js/script.js
+++ src/js/script.js
@@ -27,8 +27,7 @@ video.src = playlist[currentIndex];
 
 // display total duration
 video.addEventListener('loadedmetadata',()=>{
-  
-    durationVideo.innerHTML = video.duration.toFixed(2);
+   durationVideo.innerHTML = video.duration.toFixed(2);
     
 })
 
@@ -38,10 +37,22 @@ video.addEventListener('timeupdate', () => {
       const progress = (video.currentTime / video.duration) * 100; 
   videoOverlay.style.width = `${progress}%`;  
   if(video.currentTime == video.duration){
-pause.style.display = 'block';
-     play.style.display = 'none';
+pauseicon();
   }
 });
+
+
+function pauseicon() {
+pause.style.display = 'block';
+     play.style.display = 'none';
+}
+
+function playicon() {
+play.style.display = 'block';
+     pause.style.display = 'none';
+}
+
+
 
 // Go to the next video in the playlist
 forward.addEventListener("click", () => {
@@ -49,8 +60,7 @@ forward.addEventListener("click", () => {
   video.src = playlist[currentIndex];
   video.play();
   if(video.play()){
-    play.style.display = 'block';
-     pause.style.display = 'none';
+    playicon();
 }
  
 });
@@ -61,41 +71,47 @@ backward.addEventListener("click", () => {
   video.src = playlist[currentIndex];
    video.play();
    if(video.play()){
-    play.style.display = 'block';
-     pause.style.display = 'none';
+    playicon();
 }
   
 });
 
 // Play video when pause icon is clicked
 pause.addEventListener('click',()=>{
-    play.style.display = 'block';
-     pause.style.display = 'none';
+    playicon();
      video.play();
      
 
 })
 // pause video when Play icon is clicked
 play.addEventListener('click',()=>{
-    pause.style.display = 'block';
-     play.style.display = 'none';
+   pauseicon();
      video.pause();
 
 })
 // Stop video and reset
 shape.addEventListener('click', ()=>{
     video.load()
-     pause.style.display = 'block';
-     play.style.display = 'none';
+    pauseicon();
 videoOverlay.style.width = '0%';
 })
 
 // Mute button clicked
 
+function muteicon() {
+mute.style.display = 'block';
+  unmute.style.display = 'none';
+}
+
+function unmuteicon() {
+mute.style.display = 'none';
+  unmute.style.display = 'block';
+}
+
+
 mute.addEventListener('click', () => {
     
-  mute.style.display = 'none';
-  unmute.style.display = 'block';
+  unmuteicon();
 
     soundOverlay.style.width = `${video.volume * 100}%`; 
 
@@ -105,8 +121,7 @@ mute.addEventListener('click', () => {
 
 unmute.addEventListener('click', () => {
   
-  mute.style.display = 'block';
-  unmute.style.display = 'none';
+  muteicon();
    soundOverlay.style.width ='0%'
 });
 // full screen mode
@@ -118,7 +133,7 @@ combined.addEventListener('click', ()=>{
 
 )
 
-// click video progress bsr
+// click video progress bar
 progressBar.addEventListener("click", (e) => {
     const rect = progressBar.getBoundingClientRect();
 
@@ -131,19 +146,17 @@ progressBar.addEventListener("click", (e) => {
 // display volume icon
 
 if(video.volume == 0) {
-    mute.style.display = 'block';
-  unmute.style.display = 'none';
+    muteicon();
    soundOverlay.style.width ='0%'
 }   
 else {
         
-  mute.style.display = 'none';
-  unmute.style.display = 'block';
+  unmuteicon();
 
     soundOverlay.style.width = `${video.volume * 100}%`; 
 
 } 
-// click sound progress bsr
+// click sound progress bar
 
 soundBar.addEventListener("click", (e) => {
     const rect = soundBar.getBoundingClientRect();
@@ -155,11 +168,12 @@ video.volume = percent
     soundOverlay.style.width = `${percent * 100}%`;
 
    if (percent <= 0.01) {
-    mute.style.display = 'block';
-    unmute.style.display = 'none';
+    muteicon();
 } else {
-    mute.style.display = 'none';
-    unmute.style.display = 'block';
+  unmuteicon();
 }
 
  });
+
+
+ 
